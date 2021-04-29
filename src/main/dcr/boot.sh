a30107e05415c20ae5d70747710f1cfdabce372e
Anton Golub <mailbox@antongolub.ru>
2021-04-29 21:05:59 +0300

    refactor: separate flp-app.conf module

--- src/main/dcr/boot.sh
+++ src/main/dcr/boot.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -e
 
-for template in $NGINX_MODULE/*.conf.template; do
-    filename=${template##*/}
-    filebase=${filename%.conf.template}
+for template in $NGINX_DIR/**/*.conf.template $NGINX_DIR/*.conf.template; do
+    output=${template%.template}
 
-    envsubst '$PORT $APP_DIR $LOGS_DIR $ENVIRONMENT_PROFILE_NAME $NGINX_MODULE $ENVIRONMENT_PRODUCTS_NAME $DATACENTER' < $template > $NGINX_MODULE/$filebase.conf
+    envsubst '$PORT $APP_DIR $LOGS_DIR $ENVIRONMENT_PROFILE_NAME $NGINX_MODULE $ENVIRONMENT_PRODUCTS_NAME $DATACENTER' < $template > $output
+    echo "$template -> $output"
+
+    cat $output
+
+    rm $template
 done
 
 # run nginx
