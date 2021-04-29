b94cd47e23a401b9d4a3c1d08b6b83afb527a9ea
Anton Golub <golub.anton@gmail.com>
2021-04-29 17:59:10 +0300

    build: add initial build infra (#1)

---
+++ src/main/dcr/boot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+for template in $NGINX_MODULE/*.conf.template; do
+    filename=${template##*/}
+    filebase=${filename%.conf.template}
+
+    envsubst '$PORT $APP_DIR $LOGS_DIR $ENVIRONMENT_PROFILE_NAME $NGINX_MODULE $ENVIRONMENT_PRODUCTS_NAME $DATACENTER' < $template > $NGINX_MODULE/$filebase.conf
+done
+
+# run nginx
+nginx
